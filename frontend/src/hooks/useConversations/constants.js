cbf32d19f52a2f0f9a4deee39d5ffee0e5b321c9
shubharthaksangharsha <20bcs6872@cuchd.in>
2025-06-07 08:18:33 +0930

    Fix MAX_STORAGE_BYTES export in useConversations/constants.js

--- frontend/src/hooks/useConversations/constants.js
+++ frontend/src/hooks/useConversations/constants.js
@@ -4,6 +4,9 @@
 
 import { MAX_LOCALSTORAGE_SIZE_MB, BYTES_PER_MB, MAX_STORAGE_BYTES } from '../common-constants';
 
+// Re-export storage constants for backward compatibility
+export { MAX_LOCALSTORAGE_SIZE_MB, BYTES_PER_MB, MAX_STORAGE_BYTES };
+
 // LocalStorage keys
 export const LS_KEY_CONVERSATIONS = 'conversations';
 
